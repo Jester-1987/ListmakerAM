d4c522e50d733c424b9681abdd5ff3631cbd1701
Nicole Groeger <nicole.groeger@triosstudent.com>
2024-08-13 08:45:37 -0400

    Display Initial List

--- app/src/main/java/com/triosng/listmakeram/ui/main/ListSelectionViewHolder.kt
+++ app/src/main/java/com/triosng/listmakeram/ui/main/ListSelectionViewHolder.kt
@@ -1,6 +1,7 @@
 package com.triosng.listmakeram.ui.main
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.listmakeram.databinding.ListSelectionViewHolderBinding
 
 class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding) :
     RecyclerView.ViewHolder(binding.root) {
